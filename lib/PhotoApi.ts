486bbf003c651a310c48012ac6c852319c032e67
김소연 <ksy9252@naver.com>
2025-06-04 15:11:56 +0900

    Fix : Api 수정

--- lib/PhotoApi.ts
+++ lib/PhotoApi.ts
@@ -1,20 +1,14 @@
 import { ImageProps } from "@/types/photos";
 import axios from "axios";
 
-// 모든 이미지 불러오는 API로 수정했습니다.
-export const allPhotos = async () => {
+export const getPhotos = async () => {
   const res = await axios.get<ImageProps[]>(`https://picsum.photos/v2/list`);
-
-  return res.data;
+  return res;
 };
 
-
-export const getPhotos = async() => {
-  const res = await axios.get<ImageProps[]>(`https://picsum.photos/v2/list`);
+export const getPhoto = async (id: string) => {
+  const res = await axios.get<ImageProps[]>(
+    `https://picsum.photos/id/${id}/info`
+  );
   return res;
-}
-
-export const getPhoto = async(id: string) => {
-  const res = await axios.get<ImageProps[]>(`https://picsum.photos/id/${id}/info`);
-  return res;
-}
+};
