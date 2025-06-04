7eea26e7e2eced5de9753419928834e45ab118d1
조연경 <sig.yg5057@gmail.com>
2025-06-04 14:10:53 +0900

    Feat: login 기능 완성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 
@@ -6,11 +9,12 @@ export default function Login() {
   const [pw, setPw] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
+  const login = useAuthStore((state) => state.login);
 
-  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (id === "team2" && pw === "1234") {
-      Login();
+      login();
       router.push("/");
     } else {
       setError("아이디 또는 비밀번호가 올바르지 않습니다.");
@@ -20,32 +24,34 @@ export default function Login() {
   return (
     <>
       <div className="max-w-md mt-10  mx-auto p-4 border border-slate-200 rounded shadow-2xl">
-        <h2>로그인</h2>
-        <div>
-          <input
-            type="text"
-            placeholder="아이디"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            className="border px-3 py-2 w-full rounded"
-          />
-        </div>
-        <div className="mb-2">
-          <input
-            type="password"
-            placeholder="비밀번호"
-            value={pw}
-            onChange={(e) => setPw(e.target.value)}
-            className="border px-3 py-2 w-full rounded"
-          />
-        </div>
-        {error && <p className="text-red-500 mb-2">{error}</p>}
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded w-full cursor-pointer"
-        >
-          로그인
-        </button>
+        <h2 className="text-2xl font-semibold mb-4">로그인</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <input
+              type="text"
+              placeholder="아이디"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              className="border px-3 py-2 w-full rounded"
+            />
+          </div>
+          <div className="mb-2">
+            <input
+              type="password"
+              placeholder="비밀번호"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+              className="border px-3 py-2 w-full rounded"
+            />
+          </div>
+          {error && <p className="text-red-500 mb-2">{error}</p>}
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded w-full cursor-pointer"
+          >
+            로그인
+          </button>
+        </form>
       </div>
     </>
   );
