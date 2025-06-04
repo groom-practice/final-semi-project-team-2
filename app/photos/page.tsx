b6a598e23284b1338a9612f2ee7ebf4781547499
Seeun Jung <seeun.jung01@gmail.com>
2025-06-04 12:33:33 +0900

    design: photos 페이지 UI 변경

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface Photo {
-import { Link } from 'next/link';
   id: string;
   author: string;
   width: number;
@@ -15,18 +15,22 @@ export default async function Page(){
   const response = await fetch("https://picsum.photos/v2/list");
   if(!response.ok){
     return(
-      <div>사진을 불러오지 못했습니다.</div>
-      const photos = (await response.json()) as Photo[];
+      notFound()
     )
   }
+  const photos = (await response.json()) as Photo[];
+
   return(
-    <div>
+    <div className="grid grid-cols-3 grid-rows-1 place-items-center gap-4 p-6">
       {photos.map((photo) => (
-        <div key={photo.id}>
+        <div key={photo.id} className="overflow-hidden hover:scale-105 transition-all rounded-2xl">
           <Link href={`/photos/${photo.id}`}>
             <Image src={photo.download_url}
-              width={600}
-              height={600} />
+              width={500}
+              height={500}
+              alt={photo.author}
+              className="h-full object-cover object-center"
+              placeholder="blur" />
           </Link>
         </div>
       ))}
