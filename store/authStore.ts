7a8898ac4eea0cc4571fe93e7afc5f8ca607f3a4
조연경 <sig.yg5057@gmail.com>
2025-06-04 13:48:50 +0900

    Feat: authStore 구현

---
+++ store/authStore.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+
+type AuthState = {
+  isLoggedIn: boolean;
+  initLogin: (isLoggedIn: boolean) => void;
+  login: () => void;
+  logout: () => void;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  isLoggedIn: false,
+  initLogin: (isLoggedIn: boolean) => {
+    set({ isLoggedIn });
+  },
+  login: () => {
+    localStorage.setItem("isLoggedIn", "true");
+    set({ isLoggedIn: true });
+  },
+  logout: () => {
+    localStorage.removeItem("isLoggedIn");
+    set({ isLoggedIn: false });
+  },
+}));
