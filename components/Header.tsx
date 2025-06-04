f2cd90bc25d0e25ceff856e3ffea13a22a8fefa2
조연경 <sig.yg5057@gmail.com>
2025-06-04 14:13:35 +0900

    Fix: import 경로변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useAuthStore } from "@/store/authStore";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Header() {
