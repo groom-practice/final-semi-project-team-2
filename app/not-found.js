bffa2854b312bdd53f3bc8e8635cd09ed6b8c99c
zero <inzero1998@gmail.com>
2025-06-05 09:34:02 +0900

    Feat: 404페이지추가

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return(
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1> 페이지를 찾을 수 없습니다.</h1>
+      <p>존재하지 않는 경로이거나 삭제된 글입니다.</p>
+    </div>
+  );
+}
